8f90a9ab129b8a0491d116cc9ce34f7fa2b9a26f
Tristen <tristen.hammon21@gmail.com>
2022-10-13 15:17:49 -0500

    commit tree f8f8f3d790c7331db09ad631c7b89aa679a05efe

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require_relative '../lib/person'
 
 RSpec.configure do |config|
-  # config here
+  class Person
+    attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+ 
+
+    def initialize (attributes)
+      attributes.each do |key, value|
+        self.send("#{key}=", value)
+      end
+    end
+  end
 end
